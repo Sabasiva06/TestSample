2d43951b4cb08924204c98ef3c84560528b672f8
Sabasiva06 <sabasiva06@gmail.com>
2022-05-21 13:25:25 +0530

    1st push

---
+++ src/org/sam/SampleTest.java
@@ -0,0 +1,16 @@
+package org.sam;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("A", "a");
+		map.put("B", "b");
+		map.put("C", "c");
+		map.put("D", "d");
+		map.put("E", "e");
+	}
+}
